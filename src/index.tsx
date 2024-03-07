e9a7edfb5fe2d99c2a9ef96e6759b922a807877c
sming156@interpark.com <sming156@interpark.com>
2024-03-08 01:02:07 +0900

    feat: styled-components의 theme 설정 진행

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from './App';
+import { theme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  black001: '#232323',
+  primary: '#B8F8FB',
+};
+
+export const theme = {
+  colors,
+};
