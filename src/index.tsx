e18c88b983ac39ae104982cb0c866b59bd7af5cb
sming156@interpark.com <sming156@interpark.com>
2024-03-08 01:30:24 +0900

    feat: global-style 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { App } from './App';
 import { theme } from './styles/theme';
+import { GlobalStyle } from './styles/global-styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
