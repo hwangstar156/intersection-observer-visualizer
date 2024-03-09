485af117d29f286f1871ec67bc4c497bd83ae39b
sming156@interpark.com <sming156@interpark.com>
2024-03-10 02:03:38 +0900

    feat: observer list widget 구현

---
+++ src/manager/observer-list/observer-item.tsx
@@ -0,0 +1,44 @@
+import { css, styled } from 'styled-components';
+import { ObserverType } from './observer-list';
+
+const Container = styled.div<{ isActive: boolean }>`
+  width: 100%;
+  padding-left: 20px;
+  color: white;
+  font-size: 18px;
+  padding: 10px 0 10px 20px;
+  cursor: pointer;
+  user-select: none;
+  background-color: ${({ theme }) => theme.colors.black001};
+
+  &:hover {
+    filter: brightness(1.3);
+  }
+
+  ${({ isActive, theme }) =>
+    isActive &&
+    css`
+      background-color: ${theme.colors.primary};
+      color: ${theme.colors.black001};
+
+      &:hover {
+        filter: none;
+      }
+    `}
+`;
+
+const Emoji = styled.span`
+  font-size: 14px;
+  margin-right: 16px;
+`;
+
+interface ObserverItemProps extends ObserverType {}
+
+export function ObserverItem({ title, isActive }: ObserverItemProps) {
+  return (
+    <Container isActive={isActive}>
+      <Emoji>📜</Emoji>
+      {title}
+    </Container>
+  );
+}
---
+++ src/manager/observer-list/observer-list.tsx
@@ -0,0 +1,25 @@
+import { styled } from 'styled-components';
+import { ObserverItem } from './observer-item';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
+
+export type ObserverType = { title: string; isActive: boolean };
+
+interface ObserverListProps {
+  items: ObserverType[];
+}
+
+export function ObserverList({ items }: ObserverListProps) {
+  return (
+    <Container>
+      {items.map((item) => (
+        <ObserverItem {...item} />
+      ))}
+    </Container>
+  );
+}
