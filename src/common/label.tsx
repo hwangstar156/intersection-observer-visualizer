be236b166a0349348573e96f7f39e8d4841c6c0d
sming156@interpark.com <sming156@interpark.com>
2024-03-11 23:46:43 +0900

    feat: label에 font-size를 받을 수 있도록 수정

--- src/common/label.tsx
+++ src/common/label.tsx
@@ -1,16 +1,18 @@
 import { LabelHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const StyledLabel = styled.label`
+const StyledLabel = styled.label<{ fontSize: number }>`
   font-size: 14px;
   color: white;
+  font-size: ${({ fontSize }) => `${fontSize}px`};
 `;
 
 interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
   width?: number;
   height: number;
+  fontSize: number;
 }
 
-export function Label({ children }: LabelProps) {
-  return <StyledLabel>{children}</StyledLabel>;
+export function Label({ children, ...args }: LabelProps) {
+  return <StyledLabel {...args}>{children}</StyledLabel>;
 }
