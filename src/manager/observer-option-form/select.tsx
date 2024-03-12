c4de3f0b49edf45ea17a26dc7a514a01181a5c65
sming156@interpark.com <sming156@interpark.com>
2024-03-12 22:56:54 +0900

    feat: observerOptionForm 스타일링 수정

--- src/manager/observer-option-form/select.tsx
+++ src/manager/observer-option-form/select.tsx
@@ -1,20 +1,30 @@
+import { SelectHTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const StyledSelect = styled.select`
+const StyledSelect = styled.select<SelectProps>`
   background-color: #666;
   color: white;
   padding: 5px;
   border-radius: 5px;
   border: 1px solid transparent;
+  width: ${({ width }) => (width ? `${width}px` : '100%')};
+  height: ${({ height }) => height && `${height}px`};
+  font-size: 14px;
 
   &:focus {
     outline: none;
   }
 `;
 
-export function Select() {
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  width?: number;
+  height: number;
+  suffix?: string;
+}
+
+export function Select({ ...args }: SelectProps) {
   return (
-    <StyledSelect name="optionUnit">
+    <StyledSelect name="optionUnit" {...args}>
       <option value="%">as percent</option>
       <option value="px">as pixel</option>
     </StyledSelect>
