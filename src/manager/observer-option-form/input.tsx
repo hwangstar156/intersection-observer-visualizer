019aae7fbf4254f5d7147c2b6fdb30ec6caf0525
sming156@interpark.com <sming156@interpark.com>
2024-03-14 00:13:07 +0900

    feat: rangeInput, commonInput의 값을 동기화하도록 설정

--- src/manager/observer-option-form/input.tsx
+++ src/manager/observer-option-form/input.tsx
@@ -1,40 +1,27 @@
-import { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { RangeInput } from '../../common/range-input';
 import { theme } from '../../styles/theme';
 
 interface InputProps {
-  initialValue: number;
   min: number;
   max: number;
   step: number;
+  value: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ initialValue, max, min, step }: InputProps) {
-  const { handleChangeRangeInput, input } = useInput({ initialValue });
-
+export function Input({ max, min, step, onChange, value }: InputProps) {
   return (
     <RangeInput
       min={min}
       max={max}
       step={step}
-      value={input}
+      value={value}
       backgroundColor={'#fff'}
       barColor={theme.colors.primary}
       buttonSize={20}
       height={4}
-      onChange={handleChangeRangeInput}
+      onChange={onChange}
     />
   );
-}
-
-function useInput({ initialValue }: Pick<InputProps, 'initialValue'>) {
-  const [input, setInput] = useState(initialValue);
-
-  const handleChangeRangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.valueAsNumber;
-
-    setInput(value);
-  };
-
-  return { input, handleChangeRangeInput };
 }
