63dcf19080e7f61e9f4b0c316f9730585debe360
sming156@interpark.com <sming156@interpark.com>
2024-03-17 04:40:26 +0900

    feat: input disabled ui 구현

--- src/common/common-input.tsx
+++ src/common/common-input.tsx
@@ -1,21 +1,27 @@
 import { InputHTMLAttributes } from 'react';
 import styled from 'styled-components';
 import { SELECT_UNIT_LIST } from '../manager/observer-option-form/constants';
-import { CssLengthUnit } from '../manager/observer-option-form/css-length-unit';
 
 interface RangeInputProps extends InputHTMLAttributes<HTMLInputElement> {
   width?: number;
   height: number;
-  suffix?: (typeof SELECT_UNIT_LIST)[number];
+  hasSuffixInput: boolean;
 }
 
 const StyledInput = styled.input<RangeInputProps>`
   outline: none;
   padding-left: 5px;
   border: 1px solid transparent;
+  border-radius: 6px;
   background: transparent;
   height: ${({ height }) => (height ? `${height}px` : '10px')};
-  width: ${({ width }) => (width ? `${width - 45}px` : '100%')};
+  width: ${({ width, hasSuffixInput }) => {
+    if (width && hasSuffixInput) {
+      return `${width - 45}px`;
+    }
+
+    return `${width}px`;
+  }};
   vertical-align: center;
 
   :focus {
@@ -23,7 +29,7 @@ const StyledInput = styled.input<RangeInputProps>`
   }
 `;
 
-const Container = styled.div<Pick<RangeInputProps, 'suffix' | 'height' | 'width'>>`
+const Container = styled.div<Pick<RangeInputProps, 'height' | 'width'>>`
   position: relative;
   display: flex;
   align-items: center;
@@ -34,14 +40,10 @@ const Container = styled.div<Pick<RangeInputProps, 'suffix' | 'height' | 'width'
   width: ${({ width }) => (width ? `${width}px` : '100%')};
   box-shadow: 0 0.4rem 1.2rem rgba(0, 0, 0, 0.4);
 
-  /* &::after {
-    overflow: hidden;
-    position: absolute;
-    top: ${({ height }) => `${(height - 21) / 2}px`};
-    right: 0.5rem;
-    transition: all 0.05s ease-in-out;
-    content: '${({ suffix }) => (suffix ? suffix : '')}';
-  } */
+  :disabled {
+    background-color: ${({ theme }) => theme.colors.primary};
+    opacity: 0.7;
+  }
 `;
 
 export function CommonInput({ children, ...args }: RangeInputProps) {
