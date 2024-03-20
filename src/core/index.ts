a4824d8a432e4cecb43c992d549d3c8820daa79c
hwangjungmin <hwangstar156@gmail.com>
2024-03-20 21:27:24 +0900

    feat: 사용자의 input을 기반으로 option을 바꿀 수 있도록 추가

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,8 @@
 // 사용자에게 제공해야될 옵션..?
 
+import { IntersectionObserverOptionFormType } from '../manager/left-navigation-bar';
+import { on } from '../util/custom-event';
+
 interface CreateRectArgs {
   x: number;
   y: number;
@@ -30,14 +33,42 @@ const intersectDecorator =
     });
   };
 
+const setOptions = (hashId: string | null) => {
+  //TODO: 제일 먼저 실행되도록 빼기
+  on<IntersectionObserverOptionFormType>(window, '@submit', (e) => {
+    const { bottom, left, right, threshold, top } = e.detail;
+    const rootMargin = `${top.value}${top.cssLengthUnit} ${right.value}${right.cssLengthUnit} ${bottom.value}${bottom.cssLengthUnit} ${left.value}${left.cssLengthUnit}`;
+
+    const customOptions = {
+      root: hashId,
+      rootMargin,
+      threshold: threshold.value,
+    };
+
+    sessionStorage.setItem(
+      '__INTERSECTION_OBSERVER_VISUALIZER_CUSTOM_OPTIONS__',
+      JSON.stringify(customOptions),
+    );
+  });
+};
+
 class IntersectionObserverVisualizer extends IntersectionObserver {
   enabled: boolean;
   entry: IntersectionObserverEntry | null;
+  customOptions: IntersectionObserverInit | { isActive: boolean };
 
   constructor(callback: IntersectionObserverCallback, options: IntersectionObserverInit) {
+    // options 파싱진행 -> 사용자 custom input을 한다면 그것을 기반으로, 없다면 기본값으로 할 수 있도록 설정
+
+    if (options.root instanceof Element) {
+      options.root.classList.add('iov-1'); // TODO: hash id로 뒤에 변경
+      setOptions('iov-1');
+    }
+
     super(intersectDecorator(callback), options);
 
     this.enabled = true;
+    this.customOptions = { root: options.root, isActive: false };
     this.entry = null;
   }
 
@@ -81,7 +112,7 @@ class IntersectionObserverVisualizer extends IntersectionObserver {
     document.body.append(div);
   };
 
-  init = () => {
+  drawRectByMessage = () => {
     window.addEventListener('message', (e) => {
       this.entry = JSON.parse(e.data);
 
