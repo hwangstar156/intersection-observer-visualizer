3be67b2ab58f7d633f37033ff6bd46412c7254c8
sming156@interpark.com <sming156@interpark.com>
2024-03-23 11:24:26 +0900

    feat: react strict mode 제거

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,10 @@ import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global-styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 );
